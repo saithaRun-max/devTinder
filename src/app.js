0f33950f0edb1b9a555597c396fdd8d8a89e8eb9
Sai Tharun <128915167+saithaRun-max@users.noreply.github.com>
2024-12-19 17:24:01 +0530

    handling diffrent params

--- src/app.js
+++ src/app.js
@@ -2,25 +2,68 @@ const express = require("express");
 
 const app = express();
 
+//IMP
+
+//regex
+// /a/ if 'a' in route or 'rat' it is responded
+// /.*fly$/  - if fly available route it will responded fly & butterfly
+
+//adding query param and reading
+
+// /user?userId=101
+// console.log(req.query)
+
+// 2 or more params
+// /user?userId=101&password=testing
+// receving same
+
+// Handling -  /user/101
+
+
+
+
+// Handling query params
+
+// for /user?userId=101
+
 app.get("/user", (req, res) => {
+  console.log(req.query)
   res.send({ firstName: "Sai", lastName: "Tharun" });
 });
 
-app.post("/user", (req, res) => {
-  res.send("Data reseved sucessfully");
+// for /user?userId=101&password=testing
+
+app.get("/user", (req, res) => {
+  console.log(req.query)
+  res.send({ firstName: "Sai", lastName: "Tharun" });
 });
 
-app.patch("/user", (req, res) => {
-  res.send("Data updated sucessfully");
+// for /user/101
+
+
+// Sending - "/user/101" - Reading /user/:userId - console.log(req.params)
+
+
+app.get("/user/:userId", (req, res) => {
+  console.log(req.params)
+  res.send({ firstName: "Sai", lastName: "Tharun" });
 });
 
-app.delete("/user", (req, res) => {
-  res.send("Deleted sucessfully");
+
+// for /user/101/saitharun/pass
+
+// Sending - /user/101/saitharun/pass - Reading - "/user/:userId/:name/:password"  - console.log(req.params)
+
+
+app.get("/user/:userId/:name/:password", (req, res) => {
+  console.log(req.params)
+  res.send({ firstName: "Sai", lastName: "Tharun" });
 });
 
-// app.use("/", (req, res) => {
-//   res.send("from Dashboard");
-// });
+
+
+
+
 
 app.listen(3000, () => {
   console.log("server is running , 3000.. ");
