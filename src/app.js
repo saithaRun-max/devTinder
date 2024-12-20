d4e7f13e11bf50c22142342ef9e9a66d1b5844ac
Sai Tharun <128915167+saithaRun-max@users.noreply.github.com>
2024-12-20 17:46:43 +0530

    wildcard-normalError-handling

--- src/app.js
+++ src/app.js
@@ -1,20 +1,29 @@
 const express = require("express");
 
-const { adminAuth } = require("./middlewares/auth");
-
-const { userHistory } = require("./middlewares/history");
-
 const app = express();
 
-app.use("/admin", adminAuth);
-
-app.use("/admin/getuser", (req, res) => {
-  res.send("user data generated");
+//good way
+app.use("/getUserData", (req, res) => {
+  try {
+    //if error to getting data from db
+    throw new Error("eeeee");
+  } catch (err) {
+    res.status(500).send("Some error");
+  }
 });
 
-app.use("/user/history",userHistory, (req, res)=>{
-  res.send("Your history")
-})
+//wild card error handling
+
+//keep it last, it matchs every rotue
+
+app.use("/", (err, req, res, next) => {
+  // (2 - parameters - 1st is req, 2nd is res )
+  // (3 - parameters - 1-req, 2-res, 3-next )
+  // (4 - param - 1-error, 2-req, 3-res, 4-next)
+  if (err) {
+    res.status(500).send("Something went worng");
+  }
+});
 
 app.listen(3000, () => {
   console.log("server is running , 3000.. ");
