a243ee9982019386330a066980aef5e38abdb3cc
Sai Tharun <128915167+saithaRun-max@users.noreply.github.com>
2024-12-26 16:45:12 +0530

    Get, patch, delete methos handling

--- src/app.js
+++ src/app.js
@@ -5,6 +5,61 @@ const User = require("./models/user");
 const app = express();
 app.use(express.json());
 
+//update data of user
+app.patch("/user", async (req, res) => {
+  const userId = req.body.userId;
+  const data = req.body;
+  try {
+    const user = await User.findByIdAndUpdate({ _id: userId }, data);
+    res.send("Updated Sucessfully");
+  } catch (err) {
+    res.status(400).send("Something went wrong");
+  }
+});
+
+//delete user by id
+app.delete("/user", async (req, res) => {
+  const userId = req.body.userId;
+  try {
+    const user = await User.findByIdAndDelete(userId);
+    if (!user) {
+      res.status(401).send("user not found");
+    } else {
+      res.send("deleted sucessfully");
+    }
+  } catch (err) {
+    res.status(400).send("Something went wrong" + err);
+  }
+});
+
+//get one-user by matching email id
+app.get("/user", async (req, res) => {
+  const userEmail = req.body.emailId;
+
+  try {
+    const user = await User.find({ emailId: userEmail });
+    res.send(user);
+  } catch (err) {
+    res.status(400).send("Something went wrong");
+  }
+});
+
+//get all users
+
+app.get("/users", async (req, res) => {
+  try {
+    const users = await User.find({});
+
+    if (!users) {
+      res.send("users not found");
+    } else {
+      res.send(users);
+    }
+  } catch (err) {
+    res.status(401).send("Something went wrong");
+  }
+});
+
 app.post("/signup", async (req, res) => {
   const user = new User(req.body);
 
